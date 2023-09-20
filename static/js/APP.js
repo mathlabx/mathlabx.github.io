b7fd0f026575b95d0d4c33b939baa7d8e93ac689
mathlabx <145144410+mathlabx@users.noreply.github.com>
2023-09-19 21:49:11 -0700

    -s

--- static/js/APP.js
+++ static/js/APP.js
@@ -4,11 +4,12 @@ APP.liked = new Object();
 APP.account = new Object();
 APP.login = false;
 APP.get_from_server = function () {
-var loco_email = localStorage.getItem("User_Email");
-if (loco_email) {
+var loco_name = localStorage.getItem("User_Name");
+if (loco_name) {
   serverStorage.getItem("User", loco_email, (data) => {
     console.log(data);
     localStorage.setItem("User_Name", data.Name);
+    localStorage.setItem("User_Email", data.Email);
     localStorage.setItem("User_Info", data.Info);
   });
 }
--- static/js/login.js
+++ static/js/login.js
@@ -1,16 +1,21 @@
 function login() {
-    var userinput_email = document.getElementById("l_email").value;
+    var userinput_username = document.getElementById("l_username").value;
     var userinput_password = document.getElementById("l_password").value;
     let server_user = new Object();
-    serverStorage.getItem("User", userinput_email).then((data) => {
+    serverStorage.getItem("User", userinput_username).then((data) => {
         server_user = data;
         if (server_user.Password == userinput_password) {
-            alert("loi");
             localStorage.setItem("login", "true");
             localStorage.setItem("key", server_user.Key)
+            localStorage.setItem("User_Name", server_user.Name)
+            localStorage.setItem("User_Info", server_user.Info)
             localStorage.setItem("User_Email", server_user.Email)
             APP.get_from_server();
-            window.location = "../";
+            setTimeout(() => {
+                window.location = "../";
+            }, 1000);
+        } else {
+            alert("Wrong password or username...");
         }
     });
 }
@@ -19,13 +24,24 @@ function registered() {
     var userinput_email = document.getElementById("r_email").value;
     var userinput_name = document.getElementById("r_username").value;
     var userinput_password = document.getElementById("r_password").value;
-    // 使用示例
     const userData = {
         Name: userinput_name,
         Email: userinput_email,
         Password: userinput_password,
         Key: Date.parse(new Date())
     };
-
-    serverStorage.setItem("User", userinput_email, userData);
+    serverStorage.getItem("User", userinput_name).then((data) => {
+        if (data) {
+            alert("This username has already been registered, please use another username.");
+        } else if (data.Email == userinput_email) {
+            alert("This email has already been registered, please use another email.");
+        } else {
+            serverStorage.setItem("User", userinput_name, userData);
+            setTimeout(() => {
+                document.getElementById("l_username").value = userinput_name;
+                document.getElementById("l_password").value = userinput_password;
+                login();
+            }, 1500);
+        }
+    });
 }

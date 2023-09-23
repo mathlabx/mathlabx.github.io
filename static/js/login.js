61cd1a8beb02c4c90e1c5dc9a96c05bfcee83a64
mathlabx <145144410+mathlabx@users.noreply.github.com>
2023-09-23 11:20:59 -0700

    -s

--- static/js/login.js
+++ static/js/login.js
@@ -68,6 +68,7 @@ function registered() {
         return;
     }
 
+    alert(userinput_gender.value == "null");
     if (userinput_gender.value == "null") {
         markInvalidInput("r_gender", "请选择性别");
         return;

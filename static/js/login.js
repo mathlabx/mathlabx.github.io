bc1ef7d432af90f94da67620d07e26f505779453
mathlabx <145144410+mathlabx@users.noreply.github.com>
2023-09-23 11:35:42 -0700

    -s

--- static/js/login.js
+++ static/js/login.js
@@ -1,3 +1,30 @@
+function login() {
+    var userinput_username = (document.getElementById("l_username").value).toLowerCase();
+    var userinput_password = document.getElementById("l_password").value;
+    let server_user = new Object();
+    serverStorage.getItem("User", userinput_username).then((data) => {
+        server_user = data;
+        if (server_user && (server_user.Password === null || typeof server_user.Password === 'undefined')) {
+            alert("Wrong Username...");
+        } else if (server_user && server_user.Password == userinput_password) {
+            localStorage.setItem("login", "true");
+            localStorage.setItem("key", server_user.Key);
+            localStorage.setItem("User_Name", server_user.Name);
+            localStorage.setItem("User_Info", server_user.Info);
+            localStorage.setItem("User_Email", server_user.Email);
+            localStorage.setItem("User_Img", server_user.Img);
+            localStorage.setItem("User_Gender", server_user.Gender);
+            localStorage.setItem("User_Password", server_user.Password);
+            APP.get_from_server();
+            setTimeout(() => {
+                window.location = "../";
+            }, 1000);
+        } else {
+            alert("Wrong Password...");
+        }
+    });
+}
+
 function registered() {
     var userinput_email = document.getElementById("r_email").value;
     var userinput_name = document.getElementById("r_username").value;

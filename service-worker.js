1260ca0efdb9a5183090ae3b68be0927a48e4414
mathlabx <145144410+mathlabx@users.noreply.github.com>
2023-09-27 21:16:37 -0700

    -s

--- service-worker.js
+++ service-worker.js
@@ -43,19 +43,13 @@ self.addEventListener('install', function (event) {
 
 self.addEventListener('fetch', function (event) {
     event.respondWith(
-        // 检查网络连接状态
-        fetch(event.request).then(function (response) {
-            // 如果网络可用，则直接从服务器获取资源
-            if (navigator.onLine) {
+        caches.match(event.request).then(function (response) {
+            // 如果缓存中有匹配的资源，直接返回缓存
+            if (response) {
                 return response;
             }
 
-            // 否则从缓存中获取资源
-            return caches.match(event.request).then(function (cachedResponse) {
-                return cachedResponse || response;
-            });
-        }).catch(function () {
-            // 如果网络不可用并且缓存中也没有匹配的资源，则返回离线页面或其他备用内容
+            // 如果缓存中没有匹配的资源，返回离线页面或其他备用内容
             return caches.match('/offline.html'); // 替换为你的离线页面或备用内容路径
         })
     );

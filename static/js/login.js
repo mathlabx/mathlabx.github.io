dc3ee443ce79ef344849f70abec669c4d5e15ed6
mathlabx <145144410+mathlabx@users.noreply.github.com>
2023-10-02 17:35:07 -0700

    -s

--- static/js/login.js
+++ static/js/login.js
@@ -1,30 +1,47 @@
+// 用户登录函数
 function login() {
     var userinput_username = (document.getElementById("l_username").value).toLowerCase();
     var userinput_password = document.getElementById("l_password").value;
-    let server_user = new Object();
-    serverStorage.getItem("User", userinput_username).then((data) => {
-        server_user = data;
-        if (server_user && (server_user.Password === null || typeof server_user.Password === 'undefined')) {
-            alert("Wrong Username...");
-        } else if (server_user && server_user.Password == userinput_password) {
+
+    // 使用 Firebase 用户认证登录
+    firebase.auth().signInWithEmailAndPassword(userinput_username, userinput_password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            console.log("用户登录成功:", user);
+
+            // 在这里可以添加代码，处理用户登录成功后的操作
+            // 例如：在本地存储中保存用户认证状态
             localStorage.setItem("login", "true");
-            localStorage.setItem("key", server_user.Key);
-            localStorage.setItem("User_Name", server_user.Name);
-            localStorage.setItem("User_Info", server_user.Info);
-            localStorage.setItem("User_Email", server_user.Email);
-            localStorage.setItem("User_Img", server_user.Img);
-            localStorage.setItem("User_Gender", server_user.Gender);
-            localStorage.setItem("User_Password", server_user.Password);
-            /* APP.get_from_server();*/
+
+            // 获取其他用户信息并保存在本地存储中
+            serverStorage.getItem("User", userinput_username).then((data) => {
+                if (data) {
+                    localStorage.setItem("key", data.Key);
+                    localStorage.setItem("User_Name", data.Name);
+                    localStorage.setItem("User_Info", data.Info);
+                    localStorage.setItem("User_Email", data.Email);
+                    localStorage.setItem("User_Img", data.Img);
+                    localStorage.setItem("User_Gender", data.Gender);
+                    // 本地存储其他用户信息
+                }
+            });
+
+            // 重定向到其他页面
             setTimeout(() => {
                 window.location = "../";
             }, 1000);
-        } else {
-            alert("Wrong Password...");
-        }
-    });
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.error("用户登录失败:", errorMessage);
+
+            // 在这里可以处理登录失败的情况，例如显示错误消息给用户
+            alert("登录失败：" + errorMessage);
+        });
 }
 
+// 用户注册函数
 function registered() {
     var userinput_email = document.getElementById("r_email").value;
     var userinput_name = document.getElementById("r_username").value;
@@ -33,92 +50,41 @@ function registered() {
     var userinput_gender = document.getElementById("r_gender").value;
     var userinput_img = document.getElementById("r_img").value;
 
-    // Validate email format and whether it's filled
-    if (!userinput_email) {
-        markInvalidInput("r_email", "Email cannot be empty");
-        return;
-    } else if (!isValidEmail(userinput_email)) {
-        markInvalidInput("r_email", "Please enter a valid email address");
-        return;
-    }
+    // 使用 Firebase 用户认证注册
+    firebase.auth().createUserWithEmailAndPassword(userinput_email, userinput_password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            console.log("用户注册成功:", user);
 
-    // Validate username length and characters
-    if (!isValidUsername(userinput_name)) {
-        markInvalidInput("r_username", "Username can only contain alphanumeric characters and underscores");
-        return;
-    } else if (userinput_name.length < 6 || userinput_name.length > 16) {
-        markInvalidInput("r_username", "Username must be between 6 and 16 characters");
-        return;
-    }
+            // 在这里可以添加代码，处理用户注册成功后的操作
+            // 例如：在本地存储中保存用户认证状态
 
-    // Validate password complexity
-    if (!isValidPassword(userinput_password)) {
-        markInvalidInput("r_password", "Password must contain letters, both uppercase and lowercase, numbers, and at least one special character, and be at least 10 characters long");
-        return;
-    }
+            // 获取其他用户信息并保存在实时数据库中
+            const userData = {
+                Name: userinput_name,
+                Info: userinput_info,
+                Gender: userinput_gender,
+                Img: userinput_img,
+            };
 
-    if (!userinput_info) {
-        markInvalidInput("r_info", "Bio cannot be empty");
-        return;
-    }
+            // 使用 Firebase 实时数据库存储用户信息
+            serverStorage.setItem("User", userinput_username.toLowerCase(), userData);
 
-    // Validate bio character length
-    if (userinput_info.length > 60) {
-        markInvalidInput("r_info", "Bio cannot exceed 40 characters");
-        return;
-    }
+            // 注册成功后跳转或执行其他操作
+            setTimeout(() => {
+                document.getElementById("l_username").value = userinput_name;
+                document.getElementById("l_password").value = userinput_password;
+                login();
+            }, 1500);
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.error("用户注册失败:", errorMessage);
 
-    if (userinput_gender == "null") {
-        markInvalidInput("r_gender", "Please select a gender");
-        return;
-    }
-
-    // Validate and modify image URL
-    if (!isValidImageUrl(userinput_img)) {
-        markInvalidInput("r_img", "Please enter a valid image URL");
-        return;
-    } else {
-        // Use a new Image object to load and check the image
-        var img = new Image();
-        img.src = userinput_img;
-
-        img.onload = function () {
-            // The image loaded successfully, you can access its width and height
-            if (img.width > 0 && img.height > 0) {
-                // If the image has width and height, it's a valid image
-                // Proceed with registration logic here
-                const userData = {
-                    Name: userinput_name,
-                    Email: userinput_email,
-                    Password: userinput_password,
-                    Info: userinput_info,
-                    Gender: userinput_gender,
-                    Img: userinput_img,
-                    Key: Date.parse(new Date())
-                };
-
-                serverStorage.getItem("User", userinput_name.toLowerCase()).then((data) => {
-                    if (data) {
-                        alert("This username has already been registered, please use another username.");
-                    } else {
-                        serverStorage.setItem("User", (userinput_name).toLowerCase(), userData);
-                        setTimeout(() => {
-                            document.getElementById("l_username").value = userinput_name;
-                            document.getElementById("l_password").value = userinput_password;
-                            login();
-                        }, 1500);
-                    }
-                });
-            } else {
-                markInvalidInput("r_img", "The provided URL does not contain a valid image");
-            }
-        };
-
-        img.onerror = function () {
-            // An error occurred while loading the image
-            markInvalidInput("r_img", "Error loading the image from the provided URL");
-        };
-    }
+            // 在这里可以处理注册失败的情况，例如显示错误消息给用户
+            alert("注册失败：" + errorMessage);
+        });
 }
 
 // Function to validate image URL format and add "https://" if missing
@@ -201,4 +167,26 @@ window.addEventListener("load", function () {
     if (APP.login) {
         window.location = "../";
     }
-})
+});
+
+// 验证当前用户状态
+function checkCurrentUser() {
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            // 用户已登录
+            console.log("当前用户已登录:", user);
+
+            // 在这里可以添加代码，处理已登录用户的操作
+        } else {
+            // 用户未登录
+            console.log("当前用户未登录");
+
+            // 在这里可以添加代码，处理未登录用户的操作
+        }
+    });
+}
+
+// 页面加载时检查当前用户状态
+window.addEventListener("load", function () {
+    checkCurrentUser();
+});

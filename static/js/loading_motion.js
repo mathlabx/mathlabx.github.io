3fd28111a2612b8053be4e258b10683b84d2b839
MPC Lab <145144410+mathlabx@users.noreply.github.com>
2023-10-04 21:42:28 -0700

    -s

--- static/js/loading_motion.js
+++ static/js/loading_motion.js
@@ -1,8 +1,19 @@
 // Wait for the page to load
 window.addEventListener('load', function () {
     // Add 'loaded' class to the body element
-    setTimeout(() => {
-        document.body.classList.add('loaded');
-        document.body.style.overflow = 'auto';
-    }, 1500);
+    window_load(true);
 });
+
+function window_load(lodaed) {
+    if (loaded) {
+        setTimeout(() => {
+            document.body.classList.add('loaded');
+            document.body.style.overflow = 'auto';
+        }, 1500);
+    } else {
+        setTimeout(() => {
+            document.body.classList.remove('loaded');
+            document.body.style.overflow = '';
+        }, 1500);
+    }
+}

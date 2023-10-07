a85c3e9ac22d5faa7d0abd636f20f1eedb4ff568
MPC Lab <145144410+mathlabx@users.noreply.github.com>
2023-10-07 06:44:44 -0700

    -s

--- static/apps/js/x.js
+++ static/apps/js/x.js
@@ -220,23 +220,19 @@ const X_Operate = {
         div_container.append(new_start);
     },
 
-    newStep: (step) => {
+    newStep: (step, ka) => {
         let new_step = document.createElement("div");
         new_step.className = "new_step";
-    
-        try {
-            // 尝试解析数学表达式，如果成功，不会抛出错误
-            katex.parse(step);
-            
-            // 如果解析成功，将 step 添加到 new_step 作为纯文本
-            new_step.textContent = step;
-    
-            div_container.append(new_step);
-        } catch (error) {
-            // 如果解析失败，您可以在此处处理错误，例如显示错误消息
-            console.error("解析数学表达式失败:", error);
+
+        if (ka) {
+            // 使用 KaTeX 渲染数学公式
+            katex.render(step, new_step, { displayMode: true });
+        } else {
+            new_step.innerHTML = step;
         }
-    }    
+
+        div_container.append(new_step);
+    }
 };
 
 window.addEventListener("load", function () {

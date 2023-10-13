557c51cf771d63039ffbd11a131deda952309f59
MPC Lab <145144410+mathlabx@users.noreply.github.com>
2023-10-13 05:29:26 -0700

    Form

--- static/js/class.js
+++ static/js/class.js
@@ -16,12 +16,160 @@ function class_update() {
     }
 }
 
+//课堂加入，创建
+var isFormOpen = false;
+
+function toggleForm() {
+    var formContainer = document.getElementById('formContainer');
+    var toggleButton = document.getElementById('toggleButton');
+
+    if (!formContainer) {
+        openForm();
+        return;
+    }
+
+    if (isFormOpen) {
+        formContainer.style.display = 'none';
+        toggleButton.style.display = 'none';
+        toggleButton.textContent = 'Open';
+    } else {
+        formContainer.style.display = 'flex';
+        toggleButton.style.display = 'flex';
+        toggleButton.textContent = 'Close';
+    }
+
+    isFormOpen = !isFormOpen;
+}
+
+function openForm() {
+    var formContainer = document.createElement('div');
+    formContainer.id = 'formContainer';
+    formContainer.className = 'form-container';
+
+    var classroomCodeInput = createInput('Classroom 7-digit Code');
+    formContainer.appendChild(classroomCodeInput);
+
+    var classroomCodeError = document.createElement('div');
+    classroomCodeError.className = 'error';
+    formContainer.appendChild(classroomCodeError);
+
+    var submitButton = document.createElement('button');
+    submitButton.textContent = 'Join';
+    submitButton.style.padding = '10px';
+    submitButton.style.backgroundColor = '#4CAF50';
+    submitButton.style.color = 'white';
+    submitButton.style.border = 'none';
+    submitButton.style.borderRadius = '4px';
+    submitButton.style.cursor = 'pointer';
+    submitButton.onclick = validateJoin;
+    formContainer.appendChild(submitButton);
+
+    var classroomNameInput = createInput('Class name');
+    formContainer.appendChild(classroomNameInput);
+
+    var classroomNameError = document.createElement('div');
+    classroomNameError.className = 'error';
+    formContainer.appendChild(classroomNameError);
+
+    var classroomDescriptionInput = createTextArea('Class introduction');
+    formContainer.appendChild(classroomDescriptionInput);
+
+    var classroomDescriptionError = document.createElement('div');
+    classroomDescriptionError.className = 'error';
+    formContainer.appendChild(classroomDescriptionError);
+
+    var createButton = document.createElement('button');
+    createButton.style.padding = '10px';
+    createButton.style.backgroundColor = '#4CAF50';
+    createButton.style.color = 'white';
+    createButton.style.border = 'none';
+    createButton.style.borderRadius = '4px';
+    createButton.style.cursor = 'pointer';
+    createButton.textContent = 'Create';
+    createButton.onclick = validateCreate;
+    formContainer.appendChild(createButton);
+
+    document.body.appendChild(formContainer);
+
+    toggleForm();
+}
+
+function createInput(placeholder) {
+    var input = document.createElement('input');
+    input.type = 'text';
+    input.placeholder = placeholder;
+    return input;
+}
+
+function createTextArea(placeholder) {
+    var textarea = document.createElement('textarea');
+    textarea.placeholder = placeholder;
+    return textarea;
+}
+
+function validateJoin() {
+    var classroomCodeInput = document.getElementById('formContainer').children[0];
+    var classroomCode = classroomCodeInput.value;
+
+    var classroomCodeRegex = /^[0-9A-Fa-f]{7}$/;
+    var classroomCodeError = document.getElementById('formContainer').children[1];
+
+    classroomCodeError.textContent = '';
+
+    if (!classroomCodeRegex.test(classroomCode)) {
+        setInvalidInput(classroomCodeInput, 'Class code must be 7 hexadecimal characters.', classroomCodeError);
+        return false;
+    }
+
+    return true;
+}
+
+function validateCreate() {
+    var classroomNameInput = document.getElementById('formContainer').children[2];
+    var classroomDescriptionInput = document.getElementById('formContainer').children[5];
+
+    var classroomName = classroomNameInput.value;
+    var classroomDescription = classroomDescriptionInput.value;
+
+    var classroomNameRegex = /^.{5,20}$/;
+    var classroomDescriptionRegex = /^.{10,35}$/;
+
+    var classroomNameError = document.getElementById('formContainer').children[3];
+    var classroomDescriptionError = document.getElementById('formContainer').children[6];
+
+    classroomNameError.textContent = '';
+    classroomDescriptionError.textContent = '';
+
+    var isNameValid = true;
+    var isDescriptionValid = true;
+
+    if (!classroomNameRegex.test(classroomName)) {
+        setInvalidInput(classroomNameInput, 'Class name must be 5-20 characters.', classroomNameError);
+        isNameValid = false;
+    }
+
+    if (!classroomDescriptionRegex.test(classroomDescription)) {
+        setInvalidInput(classroomDescriptionInput, 'Class introduction must be 10-35 characters.', classroomDescriptionError);
+        isDescriptionValid = false;
+    }
+
+    return isNameValid && isDescriptionValid;
+}
+
+function setInvalidInput(inputElement, errorMessage, errorElement) {
+    inputElement.style.borderColor = "red";
+    inputElement.setCustomValidity(errorMessage);
+    inputElement.reportValidity();
+    errorElement.textContent = errorMessage;
+}
+
 window.addEventListener("load", function () {
     if (!APP.login) window.location = "../account/login.html";
     setTimeout(() => {
         serverStorage.getItem("User", APP.account.UID).then((data) => {
             if (data) {
                 APP.class = data.Class;
+                document.getElementById("join_button").onclick = openForm();
                 class_update();
             }
         });

0cab42a8b5147f7b2a5334a7f6f4541444f653aa
MPC Lab <145144410+mathlabx@users.noreply.github.com>
2023-10-14 08:29:06 -0700

    class

--- static/js/classroom.js
+++ static/js/classroom.js
@@ -12,14 +12,88 @@ function receiveObject() {
 const receivedClass = receiveObject();
 console.log(receivedClass);
 
+let Class_Data;
+
+function get_from_sever() {
+    async function load() {
+        const classesRef = firebase.database().ref('classes');
+        const classQuery = classesRef.orderByChild('code').equalTo(receivedClass);
+
+        const snapshot = await classQuery.once('value');
+        if (snapshot.exists()) {
+            const classData = snapshot.val();
+            const values = Object.values(classData);
+            for (const classItem of values) {
+                console.log("Class ID:", classItem.code);
+                console.log("Class name:", classItem.name);
+                console.log("Class description:", classItem.description);
+                Class_Data = classItem;
+            }
+        } else {
+            APP.log("Error: Data does not exist.");
+        }
+        if (!APP.class) APP.class = [];
+        document.getElementById("join_button").onclick = toggleForm;
+        class_update();
+    }
+    load();
+}
+
+get_from_sever();
+
+let div_container = document.getElementById("container");
+
+const Class_Operate = {
+    new_Task: (Task_Name, Due_Date, T_ID) => {
+        let new_flow_block = document.createElement("div");
+        new_flow_block.className = "flow_block";
+        let task_title = document.createElement("h2");
+        task_title.className = "task_title";
+        task_title.innerHTML = Task_Name;
+        let new_due_date = document.createElement("h3");
+        new_due_date.className = "due_date";
+        new_flow_block.append(task_title);
+        new_flow_block.append(new_due_date);
+        new_flow_block.addEventListener("click", function (T_ID) {
+            return function () {
+                console.log(T_ID);
+                let new_page_con = document.createElement("div");
+                new_page_con.className = "new_page_con";
+                let new_title = document.createElement("h1");
+                new_title.className = "new_title";
+                new_title.innerHTML = "Title";
+                let new_dis = document.createElement("new_dis");
+                new_dis.className = "new_dis";
+                new_dis.innerHTML = "disdis dis dis sjsandjknbj sdjaabdhsaweujhdbi2";
+                new_page_con.append(new_title);
+                new_page_con.append(new_dis);
+                div_container.append(new_page_con);
+            }
+        }(T_ID));
+        div_container.append(new_flow_block);
+    }
+};
+
+let page_on = "todo";
+
 function click_todo() {
-    
+    if (page_on != "todo") {
+
+    }
 }
 
 function click_people() {
-    
+    if (page_on != "people") {
+
+    }
 }
 
 function click_setting() {
-    
+    if (page_on != "setting") {
+
+    }
 }
+
+document.addEventListener("load", function () {
+    click_todo();
+});

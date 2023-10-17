49edb7ee8c6176ed54f85b98af29583223b29296
MPC Lab <145144410+mathlabx@users.noreply.github.com>
2023-10-17 07:56:06 -0700

    test

--- test/script.js
+++ test/script.js
@@ -1,4 +1,28 @@
-// Add your custom JavaScript for the exam here
+function isEmpty(obj) {
+    return Object.keys(obj).length === 0;
+}
+
+function receiveObject() {
+    const jsonStr = sessionStorage.getItem('MPCTest'); // 获取存储在 sessionStorage 中的数据
+
+    if (jsonStr) {
+        const data = JSON.parse(jsonStr); // 将 JSON 字符串转换为对象
+        sessionStorage.removeItem('MPCTest'); // 获取数据后清空 sessionStorage 中的数据
+        return data;
+    } else {
+        return {}; // 如果没有有效的数据，返回空对象
+    }
+}
+
+let Test = {
+    Test_Taker: {
+        UID: null,
+        CID: null
+    },
+    Questions: null,
+    Test_Time: null,
+    Used_Time: null
+}
 
 // Example Fullscreen API implementation
 document.addEventListener('fullscreenchange', (event) => {

32d170863b1ddf5e426d9dec0e678080f45072b8
MPC Lab <145144410+mathlabx@users.noreply.github.com>
2023-10-16 21:22:35 -0700

    -

--- test/script.js
+++ test/script.js
@@ -4,6 +4,7 @@
 document.addEventListener('fullscreenchange', (event) => {
     if (!document.fullscreenElement) {
         // User attempted to exit fullscreen, trigger appropriate action
+        console.log("已退出全屏！");
         alert("Exiting full-screen will end the exam.");
         // Add your code to handle exam ending here
     }

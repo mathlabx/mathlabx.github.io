9a5d112beb1e1a2168b654c4dfc2bbd281cc6647
MPC Lab <145144410+mathlabx@users.noreply.github.com>
2023-10-17 19:42:39 -0700

    -s

--- static/js/class.js
+++ static/js/class.js
@@ -296,7 +296,7 @@ window.addEventListener("load", function () {
     setTimeout(() => {
         serverStorage.getItem("User", APP.account.UID).then((data) => {
             if (data) {
-                if (!Array.isArray(data.Class) || !data.Class.length) data.Class = [];
+                if (!data.Class || data.Class.length === 0) data.Class = [];
                 async function load() {
                     for (let i = 0; i < data.Class.length; i++) {
                         const classesRef = firestore.collection('classes');

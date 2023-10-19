9c821f20aa7224522cc5c4da563cca29fbd3e6f9
MPC Lab <145144410+mathlabx@users.noreply.github.com>
2023-10-19 16:22:37 -0700

    -s

--- app/x/data/math/Middle_School_Mathematics/Algebra/Inequalities/Quadratic_inequality_of_one_variable.js
+++ app/x/data/math/Middle_School_Mathematics/Algebra/Inequalities/Quadratic_inequality_of_one_variable.js
@@ -13,6 +13,20 @@
 */
 
 $X.math.Middle_School_Mathematics.Algebra.Quadratic_inequality_of_one_variable = function (min, max) {
+        // Function to find the greatest common divisor (GCD) of two numbers
+        function findGCD(a, b) {
+                if (b === 0) {
+                        return a;
+                }
+                return findGCD(b, a % b);
+        }
+
+        // Function to simplify a fraction
+        function simplifyFraction(numerator, denominator) {
+                const gcd = findGCD(numerator, denominator);
+                return [numerator / gcd, denominator / gcd];
+        }
+
         const letters = "abcdefghijklmnopqrstuvwxyz";
         const operators = ["+", "-"];
         const signs = ["<", ">", "<=", ">="];
@@ -27,19 +41,34 @@ $X.math.Middle_School_Mathematics.Algebra.Quadratic_inequality_of_one_variable =
         const discriminant = coefficient2 * coefficient2 - 4 * coefficient1 * (constant - solution);
         let question = [];
         question.push("Solve the inequality: ");
-        question.push(coefficient1 + variable + "^2 " + operation1 + " " + coefficient2 + variable + " " + operation2 + " " + constant + "  " + inequality + " " + solution);
+        question.push(`${coefficient1}${variable}^2 ${operation1} ${coefficient2}${variable} ${operation2} ${constant}  ${inequality} ${solution}`);
         let answer;
         if (discriminant > 0) {
                 // Two real roots
                 const x1 = (-coefficient2 + Math.sqrt(discriminant)) / (2 * coefficient1);
                 const x2 = (-coefficient2 - Math.sqrt(discriminant)) / (2 * coefficient1);
-                answer = [x1, x2];
+
+                const x1Fraction = simplifyFraction(-coefficient2 + Math.sqrt(discriminant), 2 * coefficient1);
+                const x2Fraction = simplifyFraction(-coefficient2 - Math.sqrt(discriminant), 2 * coefficient1);
+
+                if (x1Fraction[1] === 1) {
+                        answer = `x_1 = ${x1Fraction[0]}, x_2 = ${x2Fraction[0]}`;
+                } else {
+                        answer = `x_1 = \\frac{${x1Fraction[0]}}{${x1Fraction[1]}}, x_2 = \\frac{${x2Fraction[0]}}{${x2Fraction[1]}}`;
+                }
         } else if (discriminant === 0) {
                 // One real root
                 const x = -coefficient2 / (2 * coefficient1);
-                answer = [x];
+                const xFraction = simplifyFraction(-coefficient2, 2 * coefficient1);
+
+                if (xFraction[1] === 1) {
+                        answer = `x = ${xFraction[0]}`;
+                } else {
+                        answer = `x = \\frac{${xFraction[0]}}{${xFraction[1]}}`;
+                }
         } else {
-                answer = "∅";
+                answer = "\\emptyset";
         }
+
         return [question, answer];
 }

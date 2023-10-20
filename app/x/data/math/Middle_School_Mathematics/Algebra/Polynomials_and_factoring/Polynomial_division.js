57738dfa2842ee5aa5c368abc45cde82ca358b28
MPC Lab <145144410+mathlabx@users.noreply.github.com>
2023-10-20 12:37:14 -0700

    -s

---
+++ app/x/data/math/Middle_School_Mathematics/Algebra/Polynomials_and_factoring/Polynomial_division.js
@@ -0,0 +1,16 @@
+/*
+拆括号简化(除法)
+
+输入：(最小数，最大数, 除数系数数量, 被除数系数数量, 不同种类字母数量（a-z）担保最大26)
+输出：[题干Katex格式，答案]
+
+示例：
+被除数系数数量4，除数系数数量2：
+x^3 + 2x^2 - 4x + 8    /    x + 2
+PS：题干和答案都要katex格式：\\frac{}{}
+*/
+
+$X.math.Middle_School_Mathematics.Algebra.Polynomials_division = function (min, max, umber_divisor, number_dividend, num_var) {
+
+    return [problem, answer];
+}
---
+++ app/x/data/math/Middle_School_Mathematics/Algebra/Polynomials_and_factoring/Polynomial_multiplication.js
@@ -0,0 +1,18 @@
+/*
+拆括号简化(乘法)
+
+输入：(最小数，最大数, 是否1*2（布尔）, 是否2*2（布尔）, 是否2*3（布尔）, 是否3*3（布尔）)
+PS：所有的布尔都是有可能生成该题型，加入随机池子。false的则出现的可能性为0
+输出：[题干Katex格式，答案]
+
+示例：
+1*2: 3x(4x+2y)
+2*2: (x+2)(x+1)
+2*3: (x^2+5)(x^2-11x-6)
+3*3: (x^2+4x+3)(2x^2-9x+7)
+*/
+
+$X.math.Middle_School_Mathematics.Algebra.Polynomials_multiplication = function (min, max, one_two, two_two, two_three, three_three) {
+
+    return [problem, answer];
+}

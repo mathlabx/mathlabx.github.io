56d70baf2015982fcbbfde1c8bf1f448f3c6ad38
MPC Lab <145144410+mathlabx@users.noreply.github.com>
2023-10-20 08:10:28 -0700

    -s

--- app/x/data/math/Middle_School_Mathematics/Algebra/Inequalities/Fractional_inequality.js
+++ app/x/data/math/Middle_School_Mathematics/Algebra/Inequalities/Fractional_inequality.js
@@ -13,6 +13,34 @@
 */
 
 $X.math.Middle_School_Mathematics.Algebra.Fractional_inequality = function (min, max) {
+    const letters = "abcdefghijklmnopqrstuvwxyz";
+    const coefficients = [1, 2, 3, 4, 5]; // 可以根据需要添加更多的系数选项
+    const numerator1 = Math.floor(Math.random() * (max - min + 1)) + min;
+    const denominator1 = Math.floor(Math.random() * (max - min + 1)) + min;
+    const numerator2 = Math.floor(Math.random() * (max - min + 1)) + min;
+    const denominator2 = Math.floor(Math.random() * (max - min + 1)) + min;
+    const coefficient1 = coefficients[Math.floor(Math.random() * coefficients.length)];
+    const coefficient2 = coefficients[Math.floor(Math.random() * coefficients.length)];
+    const variable = letters.charAt(Math.floor(Math.random() * letters.length));
+    const operation = ["<", ">", "\\leq", "\\geq"][Math.floor(Math.random() * 4)]; // 随机选择一个操作符
+    const reciprocalOperation = operation === "<" ? ">" : "<"; // 根据选择的操作符获取倒数的操作符
+    let question = [];
+    let answer;
+
+    // 生成问题和答案
+    if (Math.random() < 0.5) {
+        // 生成 < 或者 >
+        const inequality = operation === "<" ? ">" : "<";
+        question.push(`Solve the inequality: `);
+        question.push(`\\frac{${coefficient1}${variable}${numerator1 !== 1 ? '' : ''}}{${denominator1 !== 1 ? denominator1 : ''}} ${inequality} ${coefficient2}`);
+        answer = `${variable} ${reciprocalOperation} \\frac{${coefficient2}}{${coefficient1 * numerator1}}`;
+    } else {
+        // 生成 \leq 或者 \geq
+        const inequality = operation === "\\leq" ? "\\geq" : "\\leq";
+        question.push(`Solve the inequality: `);
+        question.push(`\\frac{${coefficient1}${variable}${numerator1 !== 1 ? '' : ''}}{${denominator1 !== 1 ? denominator1 : ''}} ${inequality} ${coefficient2}`);
+        answer = `${variable} ${operation} \\frac{${coefficient2 * denominator1 - coefficient1 * numerator1}}{${coefficient1 * denominator1}}`;
+    }
 
     return [question, answer];
 }

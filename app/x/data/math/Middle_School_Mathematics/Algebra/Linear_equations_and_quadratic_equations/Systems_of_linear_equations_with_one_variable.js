b344577e27274b6d0f83eb8817d63edd819f3671
MPC Lab <145144410+mathlabx@users.noreply.github.com>
2023-10-21 12:33:58 -0700

    BUG

--- app/x/data/math/Middle_School_Mathematics/Algebra/Linear_equations_and_quadratic_equations/Systems_of_linear_equations_with_one_variable.js
+++ app/x/data/math/Middle_School_Mathematics/Algebra/Linear_equations_and_quadratic_equations/Systems_of_linear_equations_with_one_variable.js
@@ -12,24 +12,18 @@ PS: 不会可以参考小学加减法
 
 // 一元一次方程组题目和答案生成函数
 $X.math.Middle_School_Mathematics.Algebra.Systems_of_linear_equations_with_one_variable = function (min, max, add, sub, num_equ) {
-    // 数组用于存储生成的问题
     let storedQuestions = [];
 
-    // 检查问题是否已存在的函数
     function checkIfQuestionExists(question) {
         return storedQuestions.includes(question);
     }
 
-    // 将小数转换为分数
     function convertToFractionIfDecimal(number) {
-        const tolerance = 1.0E-6; // 容差值，用于判断是否为小数
+        const tolerance = 1.0E-6;
 
         if (Math.abs(Math.round(number) - number) < tolerance) {
-            // 是整数
             return number;
         } else {
-            // 是小数，将其转换为分数
-            const tolerance = 1.0E-6; // 容差值
             let h1 = 1;
             let h2 = 0;
             let k1 = 0;
@@ -50,79 +44,62 @@ $X.math.Middle_School_Mathematics.Algebra.Systems_of_linear_equations_with_one_v
         }
     }
 
-    // 解决一元一次方程组
     function solveEquations(equations, solutions) {
-        // 创建一个对象来存储每个变量的值
         let variables = {};
 
-        // 将方程组转换为只包含一个变量的形式，并解决
         for (let i = 0; i < equations.length; i++) {
             let equation = equations[i];
             let solution = solutions[i];
 
-            // 假设每个方程只有一个变量 x，并且方程的形式为 "ax + b = c"
             let parts = equation.split(" ");
             let a = parseInt(parts[0]);
-            let operator = parts[1];
-            let b = parseInt(parts[2]);
-            let c = parseInt(parts[4]);
+            let operator = parts[2];
+            let b = parseInt(parts[4]);
+            let c = parseInt(parts[6]);
 
-            // 根据运算符计算 x 的值
             if (operator === "+") {
                 variables.x = (c - b) / a;
             } else if (operator === "-") {
-                variables.x = (c + b) / a;
+                variables.x = (b - c) / a;
             }
         }
 
         return variables.x;
     }
 
-    // 生成唯一问题
     let equation = new Array();
     let answer;
     do {
-        // 生成方程组
         let equations = [];
         let solutions = [];
 
         for (let i = 0; i < num_equ; i++) {
-            // 生成随机的方程答案
-            let factor = Math.floor(Math.random() * (max - min + 1)) + min; // 生成随机因子
+            let factor = Math.floor(Math.random() * (max - min + 1)) + min;
             let a = Math.floor(Math.random() * (max - min + 1)) + min;
             let b = Math.floor(Math.random() * (max - min + 1)) + min;
-            let c = a * factor + b; // 使用随机因子确保 c 不会太小或太大
+            let c = a * factor + b;
 
-
-            // 根据参数确定运算符
             let operator = add ? '+' : sub ? '-' : '+';
 
-            // 构建方程描述
             let equationDesc = `${a}x ${operator} ${b} = ${c}`;
             equations.push(equationDesc);
 
-            // 计算每个方程的答案
-            let x = add ? (c - b) / a : sub ? (c + b) / a : (c - b) / a;
+            let x = add ? (c - b) / a : sub ? (b - c) / a : (c - b) / a;
             solutions.push(x);
         }
 
-        // 构建问题描述
         equation.push("Solve the following system of linear equations of one variable.");
         for (let i = 0; i < equations.length; i++) {
             equation.push(equations[i] + ",");
         }
 
-        // 计算答案
         let xValue = solveEquations(equations, solutions);
 
-        // 根据答案是否小数，将答案表示为分数
         answer = convertToFractionIfDecimal(xValue);
 
     } while (checkIfQuestionExists(equation));
 
-    // 存储问题以避免重复
     storedQuestions.push(equation);
 
-    // 返回问题描述和答案
     return [equation, answer];
 }

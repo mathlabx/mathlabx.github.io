3205e9e9a263442382cb398b6342751a67ea8d1f
MPC Lab <145144410+mathlabx@users.noreply.github.com>
2023-10-21 20:42:40 -0700

    BUG

--- app/x/data/math/Middle_School_Mathematics/Geometry/Corresponding_angles.js
+++ app/x/data/math/Middle_School_Mathematics/Geometry/Corresponding_angles.js
@@ -6,7 +6,7 @@ $X.math.Middle_School_Mathematics.Geometry.Corresponding_angles = function () {
 
     // Generate random angle values
     let angle1 = Math.floor(Math.random() * 90) + 1;
-    let angle2 = angle1 + 60;
+    let angle2 = angle1;
 
     // Construct the question and answer
     let question = "In the given figure, if ∠1 and ∠2 are corresponding angles, and the measure of ∠1 is " + angle1 + " degrees, what is the measure of ∠2?";

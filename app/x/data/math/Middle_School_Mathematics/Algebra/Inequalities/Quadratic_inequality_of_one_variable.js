2f7e6875a5d186a55cb79a842d38145b290ae989
MPC Lab <145144410+mathlabx@users.noreply.github.com>
2023-10-21 19:56:27 -0700

    -s

--- app/x/data/math/Middle_School_Mathematics/Algebra/Inequalities/Quadratic_inequality_of_one_variable.js
+++ app/x/data/math/Middle_School_Mathematics/Algebra/Inequalities/Quadratic_inequality_of_one_variable.js
@@ -33,26 +33,26 @@ $X.math.Middle_School_Mathematics.Algebra.Quadratic_inequality_of_one_variable =
                         return findGCD(b, a % b);
                 }
 
-                var a, b, c, operator;
+                var a, b, c, discriminant;
                 do {
                         a = Math.floor(Math.random() * (max - min + 1)) + min;
                         b = Math.floor(Math.random() * (max - min + 1)) + min;
                         c = Math.floor(Math.random() * (max - min + 1)) + min;
 
-                        var operators = ['<', '>', '\\leq', '\\geq'];
-                        operator = operators[Math.floor(Math.random() * operators.length)];
+                        discriminant = b * b - 4 * a * c;
 
                         // Check if the fraction is too large and regenerate if necessary
                         var numerator = Math.abs(b);
                         var denominator = 2 * Math.abs(a);
 
                         var chanceOfNoSolution = Math.random();
-                        if (chanceOfNoSolution < 0.1) {
+                        if (chanceOfNoSolution < 0.1 && discriminant >= 0) {
                                 a = 0; // Adjust to make the equation have no solution
-                                operator = '\\emptyset'; // Set operator to indicate no solution
+                                discriminant = -1; // Set discriminant to -1 to indicate no solution
                         }
                 } while (
                         a === 0 ||
+                        discriminant < 0 ||
                         findGCD(numerator, denominator) > max
                 );
 
@@ -68,16 +68,26 @@ $X.math.Middle_School_Mathematics.Algebra.Quadratic_inequality_of_one_variable =
                 // 构造题干
                 var question = [
                         `Solve the inequality: `,
-                        `${a}x^2 ${operator} ${b}x ${operator} ${c} > 0`
+                        `${a}x^2 - ${b}x - ${c} > 0`
                 ];
 
                 // 构造答案
-                if (operator === '<' || operator === '\\leq') {
-                        answer = `x < ${simplifyFraction(b, 2 * a)} \\ or \\ x > ${simplifyFraction(c, 2 * a)}`;
-                } else if (operator === '>' || operator === '\\geq') {
-                        answer = `x \\in \\mathbb{R}`;
-                } else if (operator === '\\emptyset') {
+                if (discriminant === 0) {
+                        var x = -b / (2 * a);
+                        if (x % 1 !== 0) {
+                                // If the answer is a decimal, simplify it to a fraction
+                                answer = simplifyFraction(-b, 2 * a);
+                        } else {
+                                answer = `x < ${x} or x > ${x}`;
+                        }
+                } else if (discriminant === -1) {
                         answer = "\\emptyset";
+                } else {
+                        var x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+                        var x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+                        var simplifiedFraction1 = simplifyFraction(-b + Math.sqrt(discriminant), 2 * a);
+                        var simplifiedFraction2 = simplifyFraction(-b - Math.sqrt(discriminant), 2 * a);
+                        answer = `x < ${simplifiedFraction2} or x > ${simplifiedFraction1}`;
                 }
 
                 if (answer != "" && answer.length <= 40) valid = true;

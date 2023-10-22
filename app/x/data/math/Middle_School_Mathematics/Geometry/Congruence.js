8b65e7a271ee682f0432f931b83f338d4664472a
MPC Lab <145144410+mathlabx@users.noreply.github.com>
2023-10-21 21:27:58 -0700

    -s

--- app/x/data/math/Middle_School_Mathematics/Geometry/Congruence.js
+++ app/x/data/math/Middle_School_Mathematics/Geometry/Congruence.js
@@ -1,7 +1,130 @@
 /*相似图形*/
 
-$X.math.Middle_School_Mathematics.Geometry.Congruence = function () {
+$X.math.Middle_School_Mathematics.Geometry.Similarity = function (C_side, C_angle, triangle, square, rectangle, parallelogram, trapezoid, rhombus) {
+    var shapes = {};
 
-    // Return the question and answer in an array
-    return [question, answer];
-}
+    const shapeArray = [triangle, square, rectangle, parallelogram, trapezoid, rhombus];
+    let trueCount = shapeArray.filter(shape => shape).length;
+
+    if (trueCount > 1) {
+        const trueIndices = shapeArray.map((shape, index) => shape ? index : -1).filter(index => index !== -1);
+        const randomIndexToKeep = trueIndices[Math.floor(Math.random() * trueIndices.length)];
+        for (let i = 0; i < shapeArray.length; i++) {
+            if (i !== randomIndexToKeep) {
+                shapeArray[i] = false;
+            }
+        }
+    }
+
+    [triangle, square, rectangle, parallelogram, trapezoid, rhombus] = shapeArray;
+
+    // Logic to randomly select the shapes based on the provided boolean values
+    if (C_side && C_angle) {
+        var randomZeroOrOne = Math.floor(Math.random() * 2);
+        if (randomZeroOrOne == 0) {
+            side();
+        } else {
+            angle();
+        }
+    } else {
+        if (C_side) {
+            side();
+        }
+        if (C_angle) {
+            angle();
+        }
+    }
+
+    function side() {
+        if (triangle) {
+            // Logic for triangle when considering side
+            var scale = (Math.floor(Math.random() * 9) + 1) / 10; // Generating a random scale between 0.1 and 1.0 with one decimal place
+            var question = "Given that triangle ABC is similar to triangle DEF and AB = " + getRandomNumber() + " units, find the length of DE.";
+            var answer = "Since the triangles are similar, the ratio of corresponding sides is equal to the scale factor. Use this to find the length of DE.";
+            shapes = { type: "triangle", question: question, answer: answer };
+        } else if (square) {
+            // Logic for square when considering side
+            var scale = (Math.floor(Math.random() * 9) + 1) / 10; // Generating a random scale between 0.1 and 1.0 with one decimal place
+            var question = "Square ABCD is similar to square EFGH with a scale factor of " + scale + ". If the side length of ABCD is " + getRandomNumber() + " units, find the side length of EFGH.";
+            var answer = "Since the squares are similar, the ratio of corresponding sides is equal to the scale factor. Use this to find the side length of EFGH.";
+            shapes = { type: "square", question: question, answer: answer };
+        } else if (rectangle) {
+            // Logic for rectangle when considering side
+            var scale = (Math.floor(Math.random() * 9) + 1) / 10; // Generating a random scale between 0.1 and 1.0 with one decimal place
+            var question = "Rectangle ABCD is similar to rectangle EFGH with a scale factor of " + scale + ". If the length of ABCD is " + getRandomNumber() + " units, find the length of EFGH.";
+            var answer = "Since the rectangles are similar, the ratio of corresponding sides is equal to the scale factor. Use this to find the length of EFGH.";
+            shapes = { type: "rectangle", question: question, answer: answer };
+        } else if (parallelogram) {
+            // Logic for parallelogram when considering side
+            var scale = (Math.floor(Math.random() * 9) + 1) / 10; // Generating a random scale between 0.1 and 1.0 with one decimal place
+            var question = "Parallelogram ABCD is similar to parallelogram EFGH with a scale factor of " + scale + ". If AB = " + getRandomNumber() + " units, find EF.";
+            var answer = "Since the parallelograms are similar, the ratio of corresponding sides is equal to the scale factor. Use this to find the length of EF.";
+            shapes = { type: "parallelogram", question: question, answer: answer };
+        } else if (trapezoid) {
+            // Logic for trapezoid when considering side
+            var scale = (Math.floor(Math.random() * 9) + 1) / 10; // Generating a random scale between 0.1 and 1.0 with one decimal place
+            var question = "Trapezoid ABCD is similar to trapezoid EFGH with a scale factor of " + scale + ". If AB = " + getRandomNumber() + " units, find EF.";
+            var answer = "Since the trapezoids are similar, the ratio of corresponding sides is equal to the scale factor. Use this to find the length of EF.";
+            shapes = { type: "trapezoid", question: question, answer: answer };
+        } else if (rhombus) {
+            // Logic for rhombus when considering side
+            var scale = (Math.floor(Math.random() * 9) + 1) / 10; // Generating a random scale between 0.1 and 1.0 with one decimal place
+            var question = "Rhombus ABCD is similar to rhombus EFGH with a scale factor of " + scale + ". If the side length of ABCD is " + getRandomNumber() + " units, find the side length of EFGH.";
+            var answer = "Since the rhombuses are similar, the ratio of corresponding sides is equal to the scale factor. Use this to find the side length of EFGH.";
+            shapes = { type: "rhombus", question: question, answer: answer };
+        }
+    }
+
+    function angle() {
+        if (triangle) {
+            // Logic for triangle when considering angle
+            var scale = (Math.floor(Math.random() * 9) + 1) / 10; // Generating a random scale between 0.1 and 1.0 with one decimal place
+            var question = "Triangle ABC is similar to triangle DEF with a scale factor of " + scale + ". If angle ABC is " + getRandomAngle() + " degrees, find the measure of angle DEF.";
+            var answer = "Since the triangles are similar, the corresponding angles are equal. Use this to find the measure of angle DEF.";
+            shapes = { type: "triangle", question: question, answer: answer };
+        } else if (square) {
+            // Logic for square when considering angle
+            var scale = (Math.floor(Math.random() * 9) + 1) / 10; // Generating a random scale between 0.1 and 1.0 with one decimal place
+            var question = "Square ABCD is similar to square EFGH with a scale factor of " + scale + ". If angle ABC is " + getRandomAngle() + " degrees, find the measure of angle EFG.";
+            var answer = "Since the squares are similar, the corresponding angles are equal. Use this to find the measure of angle EFG.";
+            shapes = { type: "square", question: question, answer: answer };
+        } else if (rectangle) {
+            // Logic for rectangle when considering angle
+            var scale = (Math.floor(Math.random() * 9) + 1) / 10; // Generating a random scale between 0.1 and 1.0 with one decimal place
+            var question = "Rectangle ABCD is similar to rectangle EFGH with a scale factor of " + scale + ". If angle ABC is " + getRandomAngle() + " degrees, find the measure of angle EFG.";
+            var answer = "Since the rectangles are similar, the corresponding angles are equal. Use this to find the measure of angle EFG.";
+            shapes = { type: "rectangle", question: question, answer: answer };
+        } else if (parallelogram) {
+            // Logic for parallelogram when considering angle
+            var scale = (Math.floor(Math.random() * 9) + 1) / 10; // Generating a random scale between 0.1 and 1.0 with one decimal place
+            var question = "Parallelogram ABCD is similar to parallelogram EFGH with a scale factor of " + scale + ". If angle ABC is " + getRandomAngle() + " degrees, find the measure of angle EFG.";
+            var answer = "Since the parallelograms are similar, the corresponding angles are equal. Use this to find the measure of angle EFG.";
+            shapes = { type: "parallelogram", question: question, answer: answer };
+        } else if (trapezoid) {
+            // Logic for trapezoid when considering angle
+            var scale = (Math.floor(Math.random() * 9) + 1) / 10; // Generating a random scale between 0.1 and 1.0 with one decimal place
+            var question = "Trapezoid ABCD is similar to trapezoid EFGH with a scale factor of " + scale + ". If angle ABC is " + getRandomAngle() + " degrees, find the measure of angle EFG.";
+            var answer = "Since the trapezoids are similar, the corresponding angles are equal. Use this to find the measure of angle EFG.";
+            shapes = { type: "trapezoid", question: question, answer: answer };
+        } else if (rhombus) {
+            // Logic for rhombus when considering angle
+            var scale = (Math.floor(Math.random() * 9) + 1) / 10; // Generating a random scale between 0.1 and 1.0 with one decimal place
+            var question = "Rhombus ABCD is similar to rhombus EFGH with a scale factor of " + scale + ". If angle ABC is " + getRandomAngle() + " degrees, find the measure of angle EFG.";
+            var answer = "Since the rhombuses are similar, the corresponding angles are equal. Use this to find the measure of angle EFG.";
+            shapes = { type: "rhombus", question: question, answer: answer };
+        }
+    }
+
+    // Helper function to generate random numbers
+    function getRandomNumber() {
+        return Math.floor(Math.random() * 10) + 1; // Generating a random number between 1 and 10
+    }
+
+    // Helper function to generate random angle values
+    function getRandomAngle() {
+        return Math.floor(Math.random() * 80) + 10; // Generating a random angle between 10 and 90 degrees
+    }
+
+    // Return the array of shapes
+    return [shapes.question, shapes.answer];
+};
--- app/x/data/math/Middle_School_Mathematics/Geometry/Similarity.js
+++ app/x/data/math/Middle_School_Mathematics/Geometry/Similarity.js
@@ -3,8 +3,22 @@
 */
 
 $X.math.Middle_School_Mathematics.Geometry.Similarity = function (C_side, C_angle, triangle, square, rectangle, parallelogram, trapezoid, rhombus) {
-    var shapes = [];
-    // Assuming shapes is an array of objects representing shapes
+    var shapes = {};
+
+    const shapeArray = [triangle, square, rectangle, parallelogram, trapezoid, rhombus];
+    let trueCount = shapeArray.filter(shape => shape).length;
+
+    if (trueCount > 1) {
+        const trueIndices = shapeArray.map((shape, index) => shape ? index : -1).filter(index => index !== -1);
+        const randomIndexToKeep = trueIndices[Math.floor(Math.random() * trueIndices.length)];
+        for (let i = 0; i < shapeArray.length; i++) {
+            if (i !== randomIndexToKeep) {
+                shapeArray[i] = false;
+            }
+        }
+    }
+
+    [triangle, square, rectangle, parallelogram, trapezoid, rhombus] = shapeArray;
 
     // Logic to randomly select the shapes based on the provided boolean values
     if (C_side && C_angle) {
@@ -28,32 +42,32 @@ $X.math.Middle_School_Mathematics.Geometry.Similarity = function (C_side, C_angl
             // Logic for triangle when considering side
             var question = "Consider a triangle ABC where AB = " + getRandomNumber() + " units and BC = " + getRandomNumber() + " units. If the lengths of AB and BC are in the ratio 2:3, find the length of AC.";
             var answer = "Let the length of AB be 2x and BC be 3x. By applying the Pythagorean theorem, we can find the length of AC.";
-            shapes.push({ type: "triangle", question: question, answer: answer });
+            shapes = { type: "triangle", question: question, answer: answer };
         } else if (square) {
             // Logic for square when considering side
             var question = "A square has a side length of " + getRandomNumber() + " units. Find its diagonal length.";
             var answer = "The diagonal of a square can be found using the Pythagorean theorem with the side length.";
-            shapes.push({ type: "square", question: question, answer: answer });
+            shapes = { type: "square", question: question, answer: answer };
         } else if (rectangle) {
             // Logic for rectangle when considering side
             var question = "Consider a rectangle with length " + getRandomNumber() + " units and width " + getRandomNumber() + " units. Find its perimeter.";
             var answer = "The perimeter of a rectangle can be found using the formula 2 * (length + width).";
-            shapes.push({ type: "rectangle", question: question, answer: answer });
+            shapes = { type: "rectangle", question: question, answer: answer };
         } else if (parallelogram) {
             // Logic for parallelogram when considering side
             var question = "In a parallelogram ABCD, AB = " + getRandomNumber() + " units and the height corresponding to AB is " + getRandomNumber() + " units. Find the area of the parallelogram.";
             var answer = "The area of a parallelogram can be found using the formula base * height.";
-            shapes.push({ type: "parallelogram", question: question, answer: answer });
+            shapes = { type: "parallelogram", question: question, answer: answer };
         } else if (trapezoid) {
             // Logic for trapezoid when considering side
             var question = "Consider a trapezoid ABCD where AB = " + getRandomNumber() + " units, CD = " + getRandomNumber() + " units, and the distance between the parallel sides is " + getRandomNumber() + " units. Find its area.";
             var answer = "The area of a trapezoid can be found using the formula (1/2) * (sum of parallel sides) * height.";
-            shapes.push({ type: "trapezoid", question: question, answer: answer });
+            shapes = { type: "trapezoid", question: question, answer: answer };
         } else if (rhombus) {
             // Logic for rhombus when considering side
             var question = "A rhombus has a side length of " + getRandomNumber() + " units. Find its perimeter.";
             var answer = "The perimeter of a rhombus can be found by multiplying the length of one side by 4.";
-            shapes.push({ type: "rhombus", question: question, answer: answer });
+            shapes = { type: "rhombus", question: question, answer: answer };
         }
     }
 
@@ -62,32 +76,32 @@ $X.math.Middle_School_Mathematics.Geometry.Similarity = function (C_side, C_angl
             // Logic for triangle when considering angle
             var question = "In triangle ABC, if angle A is " + getRandomAngle() + " degrees and the other two angles are in the ratio 2:3, find the measure of angle B and angle C.";
             var answer = "Let the other two angles be 2x and 3x. Then, apply the angle sum property of a triangle to find the measures of angles B and C.";
-            shapes.push({ type: "triangle", question: question, answer: answer });
+            shapes = { type: "triangle", question: question, answer: answer };
         } else if (square) {
             // Logic for square when considering angle
             var question = "In a square ABCD, if angle A is " + getRandomAngle() + " degrees, find the measures of the other three angles.";
             var answer = "In a square, all the angles are equal. Use the property of the square to find the measures of angles B, C, and D.";
-            shapes.push({ type: "square", question: question, answer: answer });
+            shapes = { type: "square", question: question, answer: answer };
         } else if (rectangle) {
             // Logic for rectangle when considering angle
             var question = "In a rectangle, if one of the angles is " + getRandomAngle() + " degrees, find the measures of the other three angles.";
             var answer = "In a rectangle, opposite angles are equal. Use this property to find the measures of the other angles.";
-            shapes.push({ type: "rectangle", question: question, answer: answer });
+            shapes = { type: "rectangle", question: question, answer: answer };
         } else if (parallelogram) {
             // Logic for parallelogram when considering angle
             var question = "In parallelogram ABCD, if angle A is " + getRandomAngle() + " degrees, find the measures of the other three angles.";
             var answer = "In a parallelogram, consecutive angles are supplementary. Use this property to find the measures of the other angles.";
-            shapes.push({ type: "parallelogram", question: question, answer: answer });
+            shapes = { type: "parallelogram", question: question, answer: answer };
         } else if (trapezoid) {
             // Logic for trapezoid when considering angle
             var question = "In trapezoid ABCD, if angle A is " + getRandomAngle() + " degrees, find the measures of the other three angles.";
             var answer = "Use the properties of trapezoid to find the measures of angles B, C, and D.";
-            shapes.push({ type: "trapezoid", question: question, answer: answer });
+            shapes = { type: "trapezoid", question: question, answer: answer };
         } else if (rhombus) {
             // Logic for rhombus when considering angle
             var question = "In a rhombus ABCD, if angle A is " + getRandomAngle() + " degrees, find the measures of the other three angles.";
             var answer = "In a rhombus, adjacent angles are supplementary. Use this property to find the measures of the other angles.";
-            shapes.push({ type: "rhombus", question: question, answer: answer });
+            shapes = { type: "rhombus", question: question, answer: answer };
         }
     }
 

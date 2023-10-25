c2e80ddc4eb7dcf5139f08d6aeae5eb098190aaa
MPC Lab <145144410+mathlabx@users.noreply.github.com>
2023-10-25 13:41:09 -0700

    没有计算答案

--- app/x/data/math/Middle_School_Mathematics/Algebra/Polynomials_and_factoring/Polynomial_division.js
+++ app/x/data/math/Middle_School_Mathematics/Algebra/Polynomials_and_factoring/Polynomial_division.js
@@ -1,16 +1,59 @@
 /*
-拆括号简化(除法)
+Polynomials Division
 
-输入：(最小数，最大数, 除数系数数量, 被除数系数数量, 不同种类字母数量（a-z）担保最大26)
-输出：[题干Katex格式，答案]
+Description:
+This function generates a problem of polynomial division within the given coefficient and variable constraints. It creates a polynomial division problem with a specified number of divisors and dividends.
 
-示例：
-被除数系数数量4，除数系数数量2：
-x^3 + 2x^2 - 4x + 8    /    x + 2
-PS：题干和答案都要katex格式：\\frac{}{}
+Inputs:
+- min (number): The minimum value for the coefficients.
+- max (number): The maximum value for the coefficients.
+- num_divisor (number): The number of coefficients for the divisor.
+- num_dividend (number): The number of coefficients for the dividend.
+- num_var (number): The number of different variable types (limited to the English alphabet, i.e., a-z, with a maximum of 26).
+
+Outputs:
+- Array: An array containing the problem and answer in Katex format.
+
+Example Usage:
+const result = Polynomials_division(1, 10, 2, 4, 3);
+console.log(result); // Output: ['\\frac{2x^3 + 5x^2 + 8x + 3}{x^2 + 3x + 6}', ''] // The answer needs to be computed based on the generated coefficients.
 */
 
-$X.math.Middle_School_Mathematics.Algebra.Polynomials_division = function (min, max, umber_divisor, number_dividend, num_var) {
+$X.math.Middle_School_Mathematics.Algebra.Polynomials_division = function (min, max, num_divisor, num_dividend, num_var) {
+    // Generate random coefficients for the divisor and dividend
+    var divisorCoefficients = [];
+    var dividendCoefficients = [];
+
+    for (var i = 0; i < num_divisor; i++) {
+        divisorCoefficients.push(Math.floor(Math.random() * (max - min + 1)) + min);
+    }
+
+    for (var j = 0; j < num_dividend; j++) {
+        dividendCoefficients.push(Math.floor(Math.random() * (max - min + 1)) + min);
+    }
+
+    // Generate random variable names
+    var variables = 'abcdefghijklmnopqrstuvwxyz'.split('').slice(0, num_var);
+
+    // Create the polynomial strings for the divisor and dividend
+    var divisorString = '';
+    var dividendString = '';
+
+    for (var k = divisorCoefficients.length - 1; k >= 0; k--) {
+        if (divisorCoefficients[k] !== 0) {
+            divisorString += `${divisorCoefficients[k] > 0 ? '+' : ''}${divisorCoefficients[k]}${variables[k] !== 'a' ? variables[k] : ''}^${k}`;
+        }
+    }
+
+    for (var l = dividendCoefficients.length - 1; l >= 0; l--) {
+        if (dividendCoefficients[l] !== 0) {
+            dividendString += `${dividendCoefficients[l] > 0 ? '+' : ''}${dividendCoefficients[l]}${variables[l] !== 'a' ? variables[l] : ''}^${l}`;
+        }
+    }
+
+    // Create the problem and answer in Katex format
+    var problem = `\\frac{${dividendString}}{${divisorString}}`;
+    var answer = ''; // You need to compute the answer here based on the generated coefficients
 
     return [problem, answer];
 }

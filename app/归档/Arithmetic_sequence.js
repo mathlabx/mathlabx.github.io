4953bd1c51117273925612bd91b6cc3a0ffe29d6
244196806qq <132620536+244196806qq@users.noreply.github.com>
2023-10-25 21:54:21 -0700

    Update Arithmetic_sequence.js

--- app/归档/Arithmetic_sequence.js
+++ app/归档/Arithmetic_sequence.js
@@ -1,27 +1,29 @@
-function Arithmetic_sequence(min, max, difficulty){
+function Arithmetic_sequence(min, max){
     var randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
     var randomMulti = Math.floor(Math.random() * (max - min + 1)) + min;
+    var randQuestion = Math.round(Math.random());
     let coefficients = [];
+	var question;
+	var answer = "";
     for(let i = 0; i < 10; i++){
-        coefficients.push(radomNum + i * randomMulti);
+        coefficients.push(randomNum + i * randomMulti);
     }
-    if(difficulty === "easy"){
-        var question = "What is the next term of the arithmetic sequence? \n";
-        var answer;
+    if(randQuestion == 0){
+        question = "What is the next term of the arithmetic sequence? \n";
         for(let i = 0; i < coefficients.length; i++){
             if(i%4 != 0){
                 question += coefficients[i] + ", "; 
             }else{
                 question += "________, "; 
-                answer += coefficients[i];
+                answer += coefficients[i] + ", ";
             }
         }
-    }else if(difficulty === "median"){
+    }else if(randQuestion == 1){
         var constant = Math.floor(Math.random() * (max - min + 1)) + min;
         var bValue = Math.floor(Math.random() * (max - min + 1)) + min
         var equation = "b(n) = b(n-1) + " + constant + "; b(1) = " + bValue;
         var randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
-        var question = "Find the " + randomNum + " term in the sequence.";
+        question = "Given the function " + equation + ". Find the " + randomNum + " term in the sequence.";
         var answer;
         for(let i = 1; i < randomNum; i++){
             answer = bValue + constant;

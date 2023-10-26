82caebc4c21f9a27e82a30e0e15b827d69e730a2
244196806qq <132620536+244196806qq@users.noreply.github.com>
2023-10-25 21:23:13 -0700

    Update Calculating_derivatives_and_their_applications.js

--- app/x/data/math/High_School_Mathematics/Calculus/Calculating_derivatives_and_their_applications.js
+++ app/x/data/math/High_School_Mathematics/Calculus/Calculating_derivatives_and_their_applications.js
@@ -20,6 +20,20 @@ PS: The answer should be in fractions (Katex), using GCF to calculate the simple
 
 $X.math.High_School_Mathematics.Calculus.CalculatingDerivativesAndApplications = function (C_derivative, C_application) {
 	// Your code goes here
+	function makeApproximationProblem(length){
+		const constant = Math.floor(Math.random() * 5);
+		var func = makeApproximationEquation(length, constant);
+		var question = "Find the linearization of the function " + func[0] + " at x = " + constant + " and use it to approximate f(" + (constant-.1) + ").";
+		var answer = func[1];
+		return [question, answer];
+	}
+	function makeTangentLineProblem(length){
+		const constant = Math.ceil(Math.random() * 5);
+		var func = makeTangentLineEquation(length, constant);
+		var question = "Find the tangent line to the function " + func[0] + " at x = " + constant;
+		var answer = func[1];
+		return [question, answer];
+	}
 	function makeMixEquation(func, oper, length) {
 		var equation = "Find the derivative of the function  f(x) = ";
 		var derivative = "The derivative is f'(x) = ";
@@ -64,6 +78,7 @@ $X.math.High_School_Mathematics.Calculus.CalculatingDerivativesAndApplications =
 		}
 		return [equation, derivative];
 	}
+
 	function makeApproximationEquation(length, value){
 		const functionArray = ["x", "x^2", "cos(x)", "sin(x)", "ln(x)", "tan(x)", "e^x", "log(x)", "cot(x)", "sec(x)", "csc(x)", "arctan(x)", "arccot(x)", "sinh(x)", "cosh(x)", "tanh(x)"];
 		const operatorArray = ["+", "-", "*"];
@@ -71,42 +86,153 @@ $X.math.High_School_Mathematics.Calculus.CalculatingDerivativesAndApplications =
 		var slope = 0;
 		var pointValue = 0;
 		var answer;
+		var isMinus = false;
+		var isAdd = true;
 		for(var i = 0; i < length; i++){
-		var randNum1 = Math.floor(Math.random() * functionArray.length);
-		var randNum2 = Math.floor(Math.random() * operatorArray.length);
-		if(randNum2 < 2){
-			if(i < length - 1){
-				var randNum1 = Math.floor(Math.random() * functionArray.length);
-				equation += functionArray[randNum1] + " " + operatorArray[1] + " ";
-				pointValue += calculateValue(functionArray[randNum1], value);
-				slope += calculateValue(deriveFunction(functionArray[randNum1]), value);
+			var randNum1 = Math.floor(Math.random() * functionArray.length);
+			var randNum2 = Math.floor(Math.random() * operatorArray.length);
+			if(randNum2 < 2){
+				if(isAdd){
+					pointValue += calculateValue(functionArray[randNum1], value);
+					slope += calculateDeriveValue(deriveFunction(functionArray[randNum1]), value);
+					isAdd = false;
+				}else if(isMinus){
+					pointValue -= calculateValue(functionArray[randNum1], value);
+					slope -= calculateDeriveValue(deriveFunction(functionArray[randNum1]), value);
+					isMinus = false;
+				}
+				if(i < length - 1){
+					equation += functionArray[randNum1] + " " + operatorArray[randNum2] + " ";
+					if(randNum2 == 0){isAdd = true;}
+					if(randNum2 == 1){isMinus = true;}
+				}else{
+					equation += functionArray[randNum1];
+				}
+				
+			}else if(randNum2 == 2){
+				var randNum3 = Math.floor(Math.random() * functionArray.length);
+				if(isAdd){
+					pointValue += calculateValue(functionArray[randNum1], value) * calculateValue(functionArray[randNum3], value);
+					slope += calculateValue(functionArray[randNum1], value) * calculateDeriveValue(deriveFunction(functionArray[randNum3]), value) + calculateValue(functionArray[randNum3], value) *  calculateDeriveValue(deriveFunction(functionArray[randNum3]), value);
+					isAdd = false;
+				}else if(isMinus){
+					pointValue += calculateValue(functionArray[randNum1], value) * calculateValue(functionArray[randNum3], value);
+					slope += calculateValue(functionArray[randNum1], value) * calculateDeriveValue(deriveFunction(functionArray[randNum3]), value) + calculateValue(functionArray[randNum3], value) *  calculateDeriveValue(deriveFunction(functionArray[randNum3]), value);
+					isMinus = false;
+				}
+				if(i < length - 1){
+					var randNum4 = Math.round(Math.random());
+					equation += "( " + functionArray[randNum1] + " " + operatorArray[2] + " " + functionArray[randNum3] + " ) " + operatorArray[randNum4] + " ";
+					if(randNum4 == 0){isAdd = true;}
+					if(randNum4 == 1){isMinus = true;}
+				}else{
+					equation += "( " + functionArray[randNum1] + " " + operatorArray[2] + " " + functionArray[randNum3] + " ) ";
+				}
 			}else{
-				var randNum1 = Math.floor(Math.random() * functionArray.length);
-				equation += functionArray[randNum1];
-				pointValue += calculateValue(functionArray[randNum1], value);
-				slope += calculateValue(deriveFunction(functionArray[randNum1]), value);
-			}
-		}else{
-			if(i < length - 1){
 				var randNum3 = Math.floor(Math.random() * functionArray.length);
 				var randNum4 = Math.round(Math.random());
-				equation += "( " + functionArray[randNum1] + " " + operatorArray[2] + " " + functionArray[randNum3] + " ) " + operatorArray[randNum4] + " ";
-				pointValue += calculateValue(functionArray[randNum1], value);
-				slope += calculateValue(functionArray[randNum1], value) * calculateValue(deriveFunction(functionArray[randNum3]), value) + calculateValue(functionArray[randNum3], value) *  calculateValue(deriveFunction(functionArray[randNum3]), value);
-			}else{
-				var randNum3 = Math.floor(Math.random() * functionArray.length);
-				equation += "( " + functionArray[randNum1] + " " + operatorArray[2] + " " + functionArray[randNum3] + " ) ";
-				pointValue += calculateValue(functionArray[randNum1], value);
-				slope += calculateValue(functionArray[randNum1], value) * calculateValue(deriveFunction(functionArray[randNum3]), value) + calculateValue(functionArray[randNum3], value) *  calculateValue(deriveFunction(functionArray[randNum1]), value);
+				if(isAdd){
+					pointValue += calculateValue(functionArray[randNum1], value)/calculateValue(functionArray[randNum3], value);
+					slope += (calculateValue(functionArray[randNum3], value) * calculateDeriveValue(deriveFunction(functionArray[randNum1]), value) - calculateValue(functionArray[randNum1], value) *  calculateDeriveValue(deriveFunction(functionArray[randNum1]), value))/(calculateValue(functionArray[randNum3], value) * calculateValue(functionArray[randNum3], value))
+					isAdd = false;
+				}else if(isMinus){
+					pointValue += calculateValue(functionArray[randNum1], value)/calculateValue(functionArray[randNum3], value);
+					slope += (calculateValue(functionArray[randNum3], value) * calculateDeriveValue(deriveFunction(functionArray[randNum1]), value) - calculateValue(functionArray[randNum1], value) *  calculateDeriveValue(deriveFunction(functionArray[randNum1]), value))/(calculateValue(functionArray[randNum3], value) * calculateValue(functionArray[randNum3], value))
+					isMinus = false;
+				}
+				if(i < length - 1){
+					equation += "( " + functionArray[randNum1] + " " + operatorArray[3] + " " + functionArray[randNum3] + " ) " + operatorArray[randNum4] + " ";
+					if(randNum4 == 0){isAdd = true;}
+					if(randNum4 == 1){isMinus = true;}
+				}else{
+					equation += "( " + functionArray[randNum1] + " " + operatorArray[3] + " " + functionArray[randNum3] + " ) ";
+				}
 			}
 		}
-		}
-		console.log(slope);
 		if(isFinite(slope)){
-			answer = pointValue + slope * (.1);
+			answer = pointValue - slope * (.1);
 		}
 		if(!isFinite(slope) || !isFinite(pointValue)){
 			answer = "There exist a non finite number in the approximation so it can't be solved.";
+		}
+		return [equation, answer];
+	}
+	
+	function makeTangentLineEquation(length, value){
+		const functionArray = ["x", "x^2", "cos(x)", "sin(x)", "ln(x)", "tan(x)", "e^x", "log(x)", "cot(x)", "sec(x)", "csc(x)", "arctan(x)", "arccot(x)", "sinh(x)", "cosh(x)", "tanh(x)"];
+		const operatorArray = ["+", "-", "*"];
+		var equation = "f(x) = ";
+		var slope = 0;
+		var pointValue = 0;
+		var answer;
+		var isMinus = false;
+		var isAdd = true;
+		for(var i = 0; i < length; i++){
+			var randNum1 = Math.floor(Math.random() * functionArray.length);
+			var randNum2 = Math.floor(Math.random() * operatorArray.length);
+			if(randNum2 < 2){
+				if(isAdd){
+					pointValue += calculateValue(functionArray[randNum1], value);
+					slope += calculateDeriveValue(deriveFunction(functionArray[randNum1]), value);
+					isAdd = false;
+				}else if(isMinus){
+					pointValue -= calculateValue(functionArray[randNum1], value);
+					slope -= calculateDeriveValue(deriveFunction(functionArray[randNum1]), value);
+					isMinus = false;
+				}
+				if(i < length - 1){
+					equation += functionArray[randNum1] + " " + operatorArray[randNum2] + " ";
+					if(randNum2 == 0){isAdd = true;}
+					if(randNum2 == 1){isMinus = true;}
+				}else{
+					equation += functionArray[randNum1];
+				}
+				
+			}else if(randNum2 == 2){
+				var randNum3 = Math.floor(Math.random() * functionArray.length);
+				if(isAdd){
+					pointValue += calculateValue(functionArray[randNum1], value) * calculateValue(functionArray[randNum3], value);
+					slope += calculateValue(functionArray[randNum1], value) * calculateDeriveValue(deriveFunction(functionArray[randNum3]), value) + calculateValue(functionArray[randNum3], value) *  calculateDeriveValue(deriveFunction(functionArray[randNum3]), value);
+					isAdd = false;
+				}else if(isMinus){
+					pointValue += calculateValue(functionArray[randNum1], value) * calculateValue(functionArray[randNum3], value);
+					slope += calculateValue(functionArray[randNum1], value) * calculateDeriveValue(deriveFunction(functionArray[randNum3]), value) + calculateValue(functionArray[randNum3], value) *  calculateDeriveValue(deriveFunction(functionArray[randNum3]), value);
+					isMinus = false;
+				}
+				if(i < length - 1){
+					var randNum4 = Math.round(Math.random());
+					equation += "( " + functionArray[randNum1] + " " + operatorArray[2] + " " + functionArray[randNum3] + " ) " + operatorArray[randNum4] + " ";
+					if(randNum4 == 0){isAdd = true;}
+					if(randNum4 == 1){isMinus = true;}
+				}else{
+					equation += "( " + functionArray[randNum1] + " " + operatorArray[2] + " " + functionArray[randNum3] + " ) ";
+				}
+			}else{
+				var randNum3 = Math.floor(Math.random() * functionArray.length);
+				var randNum4 = Math.round(Math.random());
+				if(isAdd){
+					pointValue += calculateValue(functionArray[randNum1], value)/calculateValue(functionArray[randNum3], value);
+					slope += (calculateValue(functionArray[randNum3], value) * calculateDeriveValue(deriveFunction(functionArray[randNum1]), value) - calculateValue(functionArray[randNum1], value) *  calculateDeriveValue(deriveFunction(functionArray[randNum1]), value))/(calculateValue(functionArray[randNum3], value) * calculateValue(functionArray[randNum3], value))
+					isAdd = false;
+				}else if(isMinus){
+					pointValue += calculateValue(functionArray[randNum1], value)/calculateValue(functionArray[randNum3], value);
+					slope += (calculateValue(functionArray[randNum3], value) * calculateDeriveValue(deriveFunction(functionArray[randNum1]), value) - calculateValue(functionArray[randNum1], value) *  calculateDeriveValue(deriveFunction(functionArray[randNum1]), value))/(calculateValue(functionArray[randNum3], value) * calculateValue(functionArray[randNum3], value))
+					isMinus = false;
+				}
+				if(i < length - 1){
+					equation += "( " + functionArray[randNum1] + " " + operatorArray[3] + " " + functionArray[randNum3] + " ) " + operatorArray[randNum4] + " ";
+					if(randNum4 == 0){isAdd = true;}
+					if(randNum4 == 1){isMinus = true;}
+				}else{
+					equation += "( " + functionArray[randNum1] + " " + operatorArray[3] + " " + functionArray[randNum3] + " ) ";
+				}
+			}
+		}
+		if(isFinite(slope)){
+			answer = "(y - " + `${pointValue.toFixed(2)}` + ") = " + `${slope.toFixed(2)}` + "(x - " + value + ")";
+		}
+		if(!isFinite(slope) || !isFinite(pointValue)){
+			answer = "There exist a non finite number in the line so it can't be written.";
 		}
 		return [equation, answer];
 	}
@@ -187,7 +313,10 @@ $X.math.High_School_Mathematics.Calculus.CalculatingDerivativesAndApplications =
 			return Math.cosh(value);
 		}else if(func === "tanh(x)"){
 			return Math.tanh(value);
-		}else if(func === "1"){ /////////////////////
+		}
+	}
+	function calculateDeriveValue(func, value){
+		if(func === "1"){ /////////////////////
 			return 1;
 		}else if(func === "2x"){
 			return 2*value;
@@ -225,7 +354,6 @@ $X.math.High_School_Mathematics.Calculus.CalculatingDerivativesAndApplications =
 			return (1/(Math.cosh(value)*Math.cosh(value)));
 		}
 	}
-
 	var question;
 	var answer;
 	if (C_derivative) {
@@ -248,7 +376,9 @@ $X.math.High_School_Mathematics.Calculus.CalculatingDerivativesAndApplications =
 			equation = output[0];
 			answer = output[1];
 		}else if(application[randApp] === "tangent line"){
-			var output = makeTangentLineProblem(functionArray, operatorArray, functionLength);
+			var output = makeTangentLineProblem(functionLength);
+			equation = output[0];
+			answer = output[1];
 		}
 	}
 	// Return the question and answer in an array

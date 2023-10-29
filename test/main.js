336f28270aceb4790c658d8ece61fa58ebe66f53
MPC Lab <145144410+mathlabx@users.noreply.github.com>
2023-10-29 08:09:35 -0700

    -s

--- test/main.js
+++ test/main.js
@@ -358,22 +358,10 @@ let Test = {
                     Used_Time: Test.Used_Time,
                     User_Answer: User_Answer
                 });
-
-                if (!doc.Task) {
-                    doc.Task = {};
-                }
-                if (!doc.Task[Task_Index]) {
-                    doc.Task[Task_Index] = {};
-                }
                 const Task = doc.Task[Task_Index];
-
-                if (!Task[Test.Test_Taker.UID]) {
-                    Task[Test.Test_Taker.UID] = {};
-                }
                 const Task_Peple = Task[Test.Test_Taker.UID];
                 Task_Peple = taskArray;
-
-                classDocRef.set(doc)
+                classDocRef.update(doc)
                     .then(() => {
                         console.log('Data has been successfully set.');
                         //location.reload(); // 刷新页面

8facf8b37fd7c7dd5a0c3f3aa3ad32db38619997
244196806qq <132620536+244196806qq@users.noreply.github.com>
2023-10-31 19:12:02 -0700

    Update Random_variables_and_probability_distributions.js

--- app/x/data/math/University_Mathematics/Probability_and_Statistics/Random_variables_and_probability_distributions.js
+++ app/x/data/math/University_Mathematics/Probability_and_Statistics/Random_variables_and_probability_distributions.js
@@ -21,36 +21,50 @@ $X.math.University_Mathematics.Probability_and_Statistics.RandomVariablesAndDist
     // Your code goes here
     if(C_discrete){    
         const concepts = ["A discrete probability distribution counts occurrences that have ___________.",
-				"Discrete distributions contrast with ___________, where outcomes can fall anywhere on a continuum.",
-				"Common examples of discrete distribution include the ________________________.",
-				"These distributions often involve statistical analyses of 'counts' or 'how many times' an ____________.",
-				"In finance, discrete distributions are used in options ________________."];
-		const blanks =["countable or finite outcomes",
-				"continuous distributions",
-				"binomial, Poisson, and Bernoulli distributions",
-				"event occurs",
-				"pricing and forecasting market shocks or recessions"];
-		const randCon = Math.floor(Math.random() * concepts.length);
-		const question = "Fill in the blank for the following concept of discrete probability distribution: " + concepts[randCon];
-		const answer = blanks[randCon];
-	    	return [question, answer];
+			"Discrete distributions contrast with ___________, where outcomes can fall anywhere on a continuum.",
+			"Common examples of discrete distribution include the ________________________.",
+			"These distributions often involve statistical analyses of 'counts' or 'how many times' an ____________.",
+			"In finance, discrete distributions are used in options ________________."];
+	const blanks =["countable or finite outcomes",
+			"continuous distributions",
+			"binomial, Poisson, and Bernoulli distributions",
+			"event occurs",
+			"pricing and forecasting market shocks or recessions"];
+	const randCon = Math.floor(Math.random() * concepts.length);
+	const question = "Fill in the blank for the following concept of discrete probability distribution: " + concepts[randCon];
+	const answer = blanks[randCon];
+	return [question, answer];
     }else if(C_continuous){
-        return null;
-    }else if(C_expectation){var question;
-		var answer;
-		const gain = Math.ceil(Math.random() * 100) + 10;
-		const lost = Math.ceil(Math.random() * 50) + gain;
-		const given = ["probability", "win/lose"];
-		const randGiven = Math.round(Math.random());
-		if(randGiven == 0){
-			const probability = Math.random();
-			question = "A local charity is hosting a game to collect funds. The cost to play the game is " + gain + " dollars. If the charity pays the winner " + lost + " dollar and the probability to win is " + `${probability.toFixed(2)}` + "%. What is the expected value for the charity winning on average?";
-			answer = "$" + (gain*(1-probability) - lost*probability);
-		}else if(randGiven == 1){
-			const bad = Math.ceil(Math.random() * 10);
-			question = "A electronic company just launch a new product and earns " + gain + " dollars for each product they sell. But they found that " + bad + " out of every 100 of their new product had some problem in it, so they decided to take back all the product that had a problem and give back the customer " + lost + " dollars. What is the expected value of profit per product?";
-			answer = "$" + `${(gain*(1-(bad/100)) - lost*(bad/100)).toFixed(2)}`;
-		}
+        const concepts = ["A ___________ distribution describes the probabilities of a continuous random variable's possible values.",
+			"A continuous random variable has an ______________ set of possible values (known as the range).",
+			"The area under the curve of a continuous random variable's PDF is used to calculate its .",
+			"Common examples of discrete distribution include the ________________________.",
+			"________________________ are essential for analyzing data and preparing a dataset for efficient algorithm training."];
+	const blanks =["continuous",
+			"infinite and uncountable",
+			"probability",
+			"Normal, Continuous Uniform, Log-Normal and Exponential",
+			"Probability Distributions"];
+	const randCon = Math.floor(Math.random() * concepts.length);
+	const question = "Fill in the blank for the following concept of continuous probability distribution: " + concepts[randCon];
+	const answer = blanks[randCon];
+	return [question, answer];
+    }else if(C_expectation){
+	var question;
+	var answer;
+	const gain = Math.ceil(Math.random() * 100) + 10;
+	const lost = Math.ceil(Math.random() * 50) + gain;
+	const given = ["probability", "win/lose"];
+	const randGiven = Math.round(Math.random());
+	if(randGiven == 0){
+		const probability = Math.random();
+		question = "A local charity is hosting a game to collect funds. The cost to play the game is " + gain + " dollars. If the charity pays the winner " + lost + " dollar and the probability to win is " + `${probability.toFixed(2)}` + "%. What is the expected value for the charity winning on average?";
+		answer = "$" + (gain*(1-probability) - lost*probability);
+	}else if(randGiven == 1){
+		const bad = Math.ceil(Math.random() * 10);
+		question = "A electronic company just launch a new product and earns " + gain + " dollars for each product they sell. But they found that " + bad + " out of every 100 of their new product had some problem in it, so they decided to take back all the product that had a problem and give back the customer " + lost + " dollars. What is the expected value of profit per product?";
+		answer = "$" + `${(gain*(1-(bad/100)) - lost*(bad/100)).toFixed(2)}`;
+	}
         // Return the question and answer in an array
         return [question, answer];
     }
